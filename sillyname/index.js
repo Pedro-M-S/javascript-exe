c779a3865ffc7d9401d6bb8d19cc14864ae25d8c
pedrosampaio <1231853@isep.ipp.pt>
2024-06-01 11:22:57 +0100

    stupid external node modules example

---
+++ sillyname/index.js
@@ -0,0 +1,9 @@
+// import generateName from "sillyname";
+//
+// let name = generateName();
+// console.log(`My name is ${name}.`);
+
+import {randomSuperhero} from "superheroes";
+
+let superhero = randomSuperhero();
+console.log(`My superhero name is ${superhero}.`);
